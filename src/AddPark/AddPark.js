e9f406ad4bba21fe63c93bfb09c77147d9668f24
Ross MacDonald <rmac2289@gmail.com>
2020-07-16 10:33:41 -0700

    addpark success message styling

--- src/AddPark/AddPark.js
+++ src/AddPark/AddPark.js
@@ -14,8 +14,8 @@ export default function AddPark() {
     const handleSuggestionSubmit = e => {
         e.preventDefault();
         SuggestionsApiService.postSuggestion(parkName, location, description)
-            .then(setSuccess(true))
             .catch(error => setError(error.error));
+            setSuccess(true)
     };
 
     const getParkName = e => setParkName(e.target.value);
@@ -25,7 +25,11 @@ export default function AddPark() {
     return (
         <div className="add-park-form">
             <form onSubmit={handleSuggestionSubmit} className="add-park">
-                {success && <p>success! your submission will be reviewed, check back soon to see if it has been posted.</p>}
+                {(error === null && success) && 
+                <div className="success-box">
+                    <p id="success">Thanks for the suggestion! Your submission will be reviewed, check back soon to see if it has been posted.
+                    </p>
+                </div>}
                 <h1 id="add-park-header">Suggest a park</h1>
                 <div className="input-container">
                     <label htmlFor="park-name">park name*</label>
